30dae8ece58db27e2d6bdf88f2099dfa719d68a5
xdariel <dariel87@gmail.com>
2020-05-02 15:11:13 -0400

    Add build folder

---
+++ build/precache-manifest.2cc7a44f013a27de245407540ae5628b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e6f43a225c5ae08764149c17acbf78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b050d073813501c36bd",
+    "url": "/static/css/main.84624fd5.chunk.css"
+  },
+  {
+    "revision": "36308eac519360b7644e",
+    "url": "/static/js/2.da77e6d7.chunk.js"
+  },
+  {
+    "revision": "6b050d073813501c36bd",
+    "url": "/static/js/main.1dc7d918.chunk.js"
+  },
+  {
+    "revision": "94b01e76fbd5b20bb0d4",
+    "url": "/static/js/runtime-main.516a18a2.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
