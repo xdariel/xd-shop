825ec77d17665b7039d19793ab78e98b8cba5949
xdariel <dariel87@gmail.com>
2020-05-04 11:41:52 -0400

    Updates

---
+++ precache-manifest.40e9f32af02549c128bbb03d4a14aae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a6cdc94e3faa58bb6bbae014e0c527",
+    "url": "/xd-shop/index.html"
+  },
+  {
+    "revision": "80cb7524fced19816961",
+    "url": "/xd-shop/static/css/main.13820d97.chunk.css"
+  },
+  {
+    "revision": "b9e106c7f8eb647ad2b8",
+    "url": "/xd-shop/static/js/2.c5f32dc4.chunk.js"
+  },
+  {
+    "revision": "80cb7524fced19816961",
+    "url": "/xd-shop/static/js/main.4f5ee6d1.chunk.js"
+  },
+  {
+    "revision": "bc29f3b671fd7986661e",
+    "url": "/xd-shop/static/js/runtime~main.bf230b0a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/xd-shop/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/xd-shop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
